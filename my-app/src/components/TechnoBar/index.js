a03ae28b2d95c3fb9c8e9a2109dca0edbd66ebe1
Morganballat <mgp.ballat@gmail.com>
2021-03-27 19:37:34 +0100

    technos logos added

--- my-app/src/components/TechnoBar/index.js
+++ my-app/src/components/TechnoBar/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-// import './style.scss';
+import './style.scss';
+
+import js from '../../resources/javascript.png';
+import html from '../../resources/html.png';
+import css from '../../resources/css.png';
+import node from '../../resources/node.png';
+import react from '../../resources/react.png';
+import redux from '../../resources/redux.png';
 
 const TechnoBar = () => (
 	<div className='techno_bar'>
-		<div>ICI une liste auto défilante des logos de techno que je connais</div>
+		<img className='techno_bar_logo-html' src={html} alt='html' />
+		<img className='techno_bar_logo' src={css} alt='css' />
+		<img className='techno_bar_logo' src={js} alt='javascript' />
+		<img className='techno_bar_logo' src={react} alt='react' />
+		<img className='techno_bar_logo' src={redux} alt='redux' />
+		<img className='techno_bar_logo-node' src={node} alt='node' />
 	</div>
 );
 export default TechnoBar;
