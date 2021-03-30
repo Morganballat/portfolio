f6d05411e621ecb733d57da40fa9ba05ae0d2d86
Morganballat <mgp.ballat@gmail.com>
2021-03-30 22:58:49 +0200

    carousel in progress

--- my-app/src/components/TechnoBar/index.js
+++ my-app/src/components/TechnoBar/index.js
@@ -10,15 +10,13 @@ import react from '../../resources/react.png';
 import redux from '../../resources/redux.png';
 
 const TechnoBar = () => (
-	<div className='bar-container'>
-		<div className='techno_bar'>
-			<img className='techno_bar_logo-html' src={html} alt='html' />
-			<img className='techno_bar_logo' src={css} alt='css' />
-			<img className='techno_bar_logo' src={js} alt='javascript' />
-			<img className='techno_bar_logo' src={react} alt='react' />
-			<img className='techno_bar_logo' src={redux} alt='redux' />
-			<img className='techno_bar_logo-node' src={node} alt='node' />
-		</div>
+	<div className='techno_bar'>
+		<img className='techno_bar_logo-html' src={html} alt='html' />
+		<img className='techno_bar_logo' src={css} alt='css' />
+		<img className='techno_bar_logo' src={js} alt='javascript' />
+		<img className='techno_bar_logo' src={react} alt='react' />
+		<img className='techno_bar_logo' src={redux} alt='redux' />
+		<img className='techno_bar_logo-node' src={node} alt='node' />
 	</div>
 );
 export default TechnoBar;
