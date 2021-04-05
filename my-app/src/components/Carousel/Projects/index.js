019869b037753031a12befae442d203e639a7bf0
Morganballat <mgp.ballat@gmail.com>
2021-04-05 16:50:12 +0200

    still carousel, start again tonight

--- my-app/src/components/Carousel/Projects/index.js
+++ my-app/src/components/Carousel/Projects/index.js
@@ -5,13 +5,18 @@ import './style.scss';
 const Projects = ({ projectsList }) => {
 	console.log(projectsList);
 	const projects = projectsList.map((project) => [
-		<div key={project.id} className='projects_carousel'>
-			<h1 className='projects_project-title'>{project.name}</h1>
-			<div
-				className='projects_project-image'
-				style={{ backgroundImage: project.image }}
-			/>
-			<p>{project.technos}</p>
+		<div>
+			<div key={project.id} className='projects_carousel'>
+				<img
+					className='projects_project-image'
+					src={project.image}
+					alt={project.name}
+				/>
+				<h1 className='projects_project-title'>{project.name}</h1>
+
+				<p>{project.technos}</p>
+			</div>
+			,
 		</div>,
 	]);
 
