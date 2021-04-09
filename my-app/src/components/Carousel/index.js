05bb7f7d2d8be8a322031af5c292e0034302ead0
Morganballat <mgp.ballat@gmail.com>
2021-04-09 14:02:11 +0200

    Found rights fonts, redo @media CSS for desktop

--- my-app/src/components/Carousel/index.js
+++ my-app/src/components/Carousel/index.js
@@ -5,7 +5,7 @@ import Projects from './Projects';
 import './style.scss';
 
 const Carousel = ({ projectsList }) => (
-	<div>
+	<div className='carousel'>
 		<div className='carousel_title'>Quelques réalisations</div>
 		<Projects projectsList={projectsList} />
 	</div>
